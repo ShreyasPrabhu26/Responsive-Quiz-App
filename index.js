8cc53d0d9c6ad4229fc9cd36773ae0bbc91fc62c
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2022-07-08 21:49:15 +0530

    Add-Questions

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello");
+const quizDta = [
+    {
+        question: "Which is the most used Programing Language in 2019?",
+        a: "Java",
+        b: "Python",
+        c: "Ruby",
+        d: "Javascript",
+        correct: "a"
+    },
+    {
+        question: "Who is the CEO of Microsoft?",
+        a: "Sundar Pichay",
+        b: "Elon Musk",
+        c: "Jeff Bezzos",
+        d: "Satya Nadella",
+        correct: "d"
+    },
+    {
+        question: "Who is the CEO of Space-X and Tesla?",
+        a: "Sundar Pichay",
+        b: "Elon Musk",
+        c: "Jeff Bezzos",
+        d: "Satya Nadella",
+        correct: "b"
+    },
+    {
+        question: "Who is the CEO of Google?",
+        a: "Sundar Pichay",
+        b: "Elon Musk",
+        c: "Jeff Bezzos",
+        d: "Satya Nadella",
+        correct: "a"
+    },
+    {
+        question: "Who is the CEO of Amazon?",
+        a: "Sundar Pichay",
+        b: "Elon Musk",
+        c: "Jeff Bezzos",
+        d: "Satya Nadella",
+        correct: "c"
+    },
+    {
+        question: "Who is the CEO of Cred?",
+        a: "Elon Musk",
+        b: "Kunal Shah",
+        c: "Jeff Bezzos",
+        d: "Satya Nadella",
+        correct: "b"
+    },
+]
